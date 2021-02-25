c11e7e0bb3ad7f25a47326f5bdd79bef8b213f8c
“Dennie” <oculareo@gmail.com>
2021-02-25 08:38:36 -0500

    Initial commit.  Project ready for React, Firebase and Tailwind CSS

---
+++ src/lib/firebase.js
@@ -0,0 +1,27 @@
+import firebase from "firebase";
+// Utilizes dotenv to retrieve relevant keys from environmental variables
+require("dotenv").config();
+
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_SENDER_ID,
+  FIREBASE_STORAGE,
+  FIREBASE_APP_ID,
+} = process.env;
+
+const config = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE,
+  messagingSenderId: FIREBASE_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+};
+
+// Initialize and export firebase
+const firebaseApp = firebase.initializeApp(config);
+const { FieldValue } = firebase.firestore;
+
+export { firebaseApp, FieldValue };
