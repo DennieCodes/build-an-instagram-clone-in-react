64ad29733d76f4aca8a6dd55ee966b1825a576be
“Dennie” <oculareo@gmail.com>
2021-02-26 12:08:06 -0500

    ADDED REACT_APP_ prefix to .env files, added seed.js function to seed database

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,30 +1,36 @@
-// import { seedDatabase } from "../seed";
-
-import firebaseApp from "firebase";
+import { seedDatabase } from "../seed";
 
 // Utilizes dotenv to retrieve relevant keys from environmental variables
 require("dotenv").config();
 
 const {
-  FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_SENDER_ID,
-  FIREBASE_STORAGE,
-  FIREBASE_APP_ID,
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE,
+  REACT_APP_FIREBASE_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
 } = process.env;
 
 const config = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE,
-  messagingSenderId: FIREBASE_SENDER_ID,
-  appId: FIREBASE_APP_ID,
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE,
+  messagingSenderId: REACT_APP_FIREBASE_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize and export firebase
-const firebase = firebaseApp.initializeApp(config);
-const { FieldValue } = firebaseApp.firestore;
+// const firebaseApp = firebase.initializeApp(config);
+// const { FieldValue } = firebaseApp.firestore();
+
+const firebase = window.firebase.initializeApp(config);
+const { FieldValue } = firebase.firestore;
+
+// console.log(`Field Value: ${FieldValue}`);
+
+// NOTE: After running once, remove or comment out the following command
+seedDatabase(firebase);
 
 export { firebase, FieldValue };
--- src/seed.js
+++ src/seed.js
@@ -2,10 +2,10 @@
 export function seedDatabase(firebase) {
   const users = [
     {
-      userId: "bmjpkU8bBWRse2JwKgzOGTv5tUI3",
-      username: "karl",
-      fullName: "Karl Hadwen",
-      emailAddress: "karlhadwen@gmail.com",
+      userId: "J1YQOOvwLpSVtMlfsdclls67tZk2",
+      username: "Oculareo",
+      fullName: "Dennie C",
+      emailAddress: "oculareo@gmail.com",
       following: ["2"],
       followers: ["2", "3", "4"],
       dateCreated: Date.now(),
@@ -16,7 +16,7 @@ export function seedDatabase(firebase) {
       fullName: "Raffaello Sanzio da Urbino",
       emailAddress: "raphael@sanzio.com",
       following: [],
-      followers: ["bmjpkU8bBWRse2JwKgzOGTv5tUI3"],
+      followers: ["J1YQOOvwLpSVtMlfsdclls67tZk2"],
       dateCreated: Date.now(),
     },
     {
@@ -25,7 +25,7 @@ export function seedDatabase(firebase) {
       fullName: "Salvador Dalí",
       emailAddress: "salvador@dali.com",
       following: [],
-      followers: ["bmjpkU8bBWRse2JwKgzOGTv5tUI3"],
+      followers: ["J1YQOOvwLpSVtMlfsdclls67tZk2"],
       dateCreated: Date.now(),
     },
     {
@@ -34,14 +34,37 @@ export function seedDatabase(firebase) {
       fullName: "George Orwell",
       emailAddress: "george@orwell.com",
       following: [],
-      followers: ["bmjpkU8bBWRse2JwKgzOGTv5tUI3"],
+      followers: ["J1YQOOvwLpSVtMlfsdclls67tZk2"],
       dateCreated: Date.now(),
     },
   ];
 
   for (let k = 0; k < users.length; k++) {
-    firebase.firestore().collection("users").add(users[k]);
+    firebase
+      .firestore()
+      .collection("users")
+      .add(users[k])
+      .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch((error) => {
+        console.error("Error adding document: ", error);
+      });
   }
+
+  // Use Get Method to test success of the upload
+  firebase
+    .firestore()
+    .collection("users")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+      });
+    })
+    .catch((error) => {
+      console.error("Error getting document:", error);
+    });
 
   for (let i = 1; i <= 5; ++i) {
     firebase
